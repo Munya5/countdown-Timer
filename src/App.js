67629938dea7a22b11dfb2ccee7ba8523c50db42
Munyaradzi <munyargumbu@gmail.com>
2023-05-18 17:50:44 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
+import React , { useEffect,useRef , useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+const App = () => { 
+  const [timerDays , setTimerDays] = useState('00');
+  const [timerHours , setTimerHours] = useState('00');
+  const [timerMinutes , setTimerMinutes] = useState('00');
+  const [timerSeconds , setTimerSeconds] = useState('00');
+
+
+  let interval = useRef();
+
+  const startTimer   = () => {
+    const countdownDate = new Date(' May 30, 2023 00:00:00 ').getTime();
+
+    interval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = countdownDate - now;
+
+      const days = Math.floor(distance /  (1000*60*60*24));
+      const hours = Math.floor(( distance %  (1000*60*60*24) / (1000*60*60)));
+      const minutes = Math.floor((distance %  (1000*60*60)/ (1000*60)));
+      const seconds = Math.floor(distance %  (1000*60)/1000);
+
+      if (distance < 0 ) {
+        clearInterval(interval.current);
+
+      } else {
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinutes(minutes);
+        setTimerSeconds(seconds)
+      }
+    }, 1000);
+
+  } 
+
+  useEffect(() => {
+      startTimer();
+      return () => {
+        clearInterval(interval.current);
+      }
+    })
+
+  return (
+    <section className='timer-container'>
+      
+      <section className='timer'>
+        <div>
+             
+        </div>
+        <div>
+          <section>
+            <p>{timerDays}</p>
+            <p><small>Days</small></p>
+
+          </section>
+          <span>:</span>
+
+          <section>
+            <p>{timerHours}</p>
+            <p><small>Hours</small></p>
+
+          </section>
+          <span>:</span>
+
+
+          <section>
+            <p>{timerMinutes}</p>
+            <p><small>Minutes</small></p>
+
+          </section>
+          <span>:</span>
+
+          <section>
+            <p>{timerSeconds}</p>
+            <p><small>Seconds</small></p>
+
+          </section>
+          
+
+
+        </div>
+
+      </section>
+    </section>
+  );
+};
+
+export default App; 
+
